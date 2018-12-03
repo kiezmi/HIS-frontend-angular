5d16fe0fb3fa9d3ec68e538f100f5c1a925b8084
Kiezmi Quinza <Kiezmi@gmail.com>
2018-12-03 14:13:52 +0100

    Actualizacion de NPM

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -1,24 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiServiceService } from '../api-service.service';
+import { Component, OnInit } from "@angular/core";
+import { ApiServiceService } from "../api-service.service";
 
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-patient',
-  templateUrl: './patient.component.html',
-  styleUrls: ['./patient.component.css']
+  selector: "app-patient",
+  templateUrl: "./patient.component.html",
+  styleUrls: ["./patient.component.css"]
 })
 export class PatientComponent implements OnInit {
-  paciente: object;
+  paciente: any;
   constructor(private api: ApiServiceService, private route: ActivatedRoute) {
     route.params.subscribe(params => {
-      let id = params.uid
+      let id = params.uid;
       this.paciente = this.api.getPatient(id);
     });
   }
 
-  ngOnInit() {
-
-  }
-
+  ngOnInit() {}
 }
